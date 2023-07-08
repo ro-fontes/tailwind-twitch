65cc72f44e754cd901657fb4d982fb51407d4e75
Sh4rk-dev <renanrapace13@gmail.com>
2023-07-07 23:33:56 -0300

    Splitting Main into components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,11 @@
 import { Layout } from "@/components/Layout";
-import { ArrowLeft, ArrowRight } from "lucide-react";
+import { MainComponent } from "@/components/Main";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <Layout>
-      <div className="flex mt-3 items-center justify-between p-6">
-        <button className="flex items-center p-2 w-9 h-9 rounded hover:bg-zinc-400/20">
-          <ArrowLeft />
-        </button>
-        <Image
-          src="/gaules.jpg"
-          width={560}
-          height={320}
-          alt="Imagem do Gaules fazendo live"
-        />
-        <button className="flex items-center p-2 w-9 h-9 rounded hover:bg-zinc-400/20">
-          <ArrowRight />
-        </button>
-      </div>
+      <MainComponent/>
     </Layout>
   );
 }
